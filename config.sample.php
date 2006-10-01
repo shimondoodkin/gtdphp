6e9dcb4dcc84bfb7c239664559a5e28d525e749b
Scott Jason Korvek <skorvek@gtd-php.com>
2006-10-01 01:59:19 +0000

    Sample confic.sample.php file--  incomplete (using my config.php file until completely built)

--- config.sample.php
+++ config.sample.php
@@ -4,6 +4,37 @@
 	$user = '<change>';
 	$pass = '<change>';
 	$title = 'GTD';
-	$theme = 'default';
+
+        $theme = "default";
+
+
+
+//Configuration settings
+
+        $config = array(
+                "db"                  => "mysql",
+                "debug"               => "true",
+                "contextsummary"      => "all"  //all | nextaction
+
+//Other configuration settings....
+                );
+
+
+
+//Default sort order for each query (can be easily overridden within each page...)
+//Once all built, can be either (a) simplified for user-editing, (b) create an options page that alters the config file, or (c) placed in the database and options page employed [best option?]
+
+//simplify all options down to a few...
+
+        $sort = array(
+
+            "projectssummary"       => "`projects`.`name` ASC",
+            "spacecontextselectbox"          => "`context`.`name` ASC",
+            "categoryselectbox"             => "`categories`.`category` ASC",
+            "projectselectbox"          => "`projects`.`name` ASC",
+            "timecontextselectbox"       => "`timeitems`.`timeframe` DESC"
+
+
+                );
 ?> 
 
