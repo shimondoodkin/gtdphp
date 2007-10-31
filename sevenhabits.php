ad0934e600910047d25d7a0baf4ac7ca65562791
Andrew Smith <gtdphp@googlemail.com>
2007-10-31 11:52:35 +0000

    Added sevenhabits.php to SVN

---
+++ sevenhabits.php
@@ -0,0 +1,10 @@
+<h3>Seven Habits of Highly Effective People</h3>
+<ol>
+    <li>Be Proactive</li>
+    <li>Begin with the End in Mind</li>
+    <li>Put First Things First</li>
+    <li>Think Win/Win</li>
+    <li>Seek First to Understand, Then to Be Understood</li>
+    <li>Synergize</li>
+    <li>Sharpen the Saw</li>
+</ol>
